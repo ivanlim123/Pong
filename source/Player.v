763bd48666fd9b9cf6aaf445989a43d09bd91b0a
Ivan Lim <36753941+ivanlim123@users.noreply.github.com>
2019-12-28 14:40:15 +0800

    Player Movement Update

--- source/Player.v
+++ source/Player.v
@@ -5,7 +5,7 @@ module Player(clk, rst, keyboard, ballX, ballY, player, posX, posY);
 
 // declare input output
 input clk, rst, player;
-input keyboard;
+input [1:0] keyboard;
 input [9:0] ballX;
 input [8:0] ballY;
 output wire [9:0] posX;
@@ -13,7 +13,7 @@ output reg [8:0] posY;
 
 reg [8:0] nextPosY;
 
-assign posX = (player == 1'b1) ? 10'd622: 9'd0;
+assign posX = (player == 1'b1) ? 10'd614: 9'd0;
 
 always @(posedge clk) begin
     if(rst==1'b1) begin
@@ -26,17 +26,21 @@ end
 
 // player movement
 always @(*) begin
-    if(keyboard==`UP) begin
-        if(|posY) begin
-            nextPosY = posY - 1'b1;
+    if(keyboard[0]==`DOWN) begin
+        if(posY < 9'd424) begin
+            nextPosY = posY + 1'b1;
         end
         else begin
             nextPosY = posY;
         end
     end
-    else if(keyboard==`DOWN) begin
-        if(posY < 9'd424) begin
-            nextPosY = posY + 1'b1;
+    else begin
+        nextPosY = posY;
+    end
+
+    if(keyboard[1]==`UP) begin
+        if(|posY) begin
+            nextPosY = posY - 1'b1;
         end
         else begin
             nextPosY = posY;
