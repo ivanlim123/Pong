bd39cea86c7aee31caf11f034ced07b158eb425d
allen9613 <59288663+allen9613@users.noreply.github.com>
2019-12-28 17:04:18 +0800

    Add state transition and fixed bug
    Use counter to slow down the movement of paddle

--- source/Player.v
+++ source/Player.v
@@ -1,55 +1,75 @@
-`define UP 1'b0
-`define DOWN 1'b1
+// state
+`define START 2'b00
+`define SERVE 2'b01
+`define PLAY 2'b10
+`define DONE 2'b11
 
-module Player(clk, rst, keyboard, ballX, ballY, player, posX, posY);
+module Player(clk, rst, state, keyboard, ballX, ballY, player, posX, posY);
 
 // declare input output
 input clk, rst, player;
+input [1:0] state;
 input [1:0] keyboard;
 input [9:0] ballX;
 input [8:0] ballY;
 output wire [9:0] posX;
 output reg [8:0] posY;
 
+reg [16:0] counter, next_counter;
 reg [8:0] nextPosY;
 
 assign posX = (player == 1'b1) ? 10'd614: 9'd0;
 
 always @(posedge clk) begin
     if(rst==1'b1) begin
-        posY <= 9'd0;
+        posY <= 9'd232;
+        counter <= 17'd0;
     end
     else begin
         posY <= nextPosY;
+        counter <= next_counter;
     end
 end
 
 // player movement
 always @(*) begin
-    if(keyboard[0]==`DOWN) begin
-        if(posY < 9'd424) begin
-            nextPosY = posY + 1'b1;
+    case(state) 
+        `START: begin
+            nextPosY = 9'd232;
+            next_counter =17'd0;
         end
-        else begin
-            nextPosY = posY;
+        `DONE: begin
+            nextPosY = 9'd232;
+            next_counter = 17'd0;
         end
-    end
-    else begin
-        nextPosY = posY;
-    end
-
-    if(keyboard[1]==`UP) begin
-        if(|posY) begin
-            nextPosY = posY - 1'b1;
+        default: begin
+            next_counter = counter + 1'b1;
+            if(counter==17'b1_1111_1111_1111_1111) begin
+                if(keyboard==2'b01) begin
+                    if(posY < 9'd424) begin
+                        nextPosY = posY + 1'b1;
+                    end
+                    else begin
+                        nextPosY = posY;
+                    end
+                end
+                else if(keyboard==2'b10) begin
+                    if(posY > 9'd0) begin
+                        nextPosY = posY - 1'b1;
+                    end
+                    else begin
+                        nextPosY = posY;
+                    end
+                end
+                else begin
+                    nextPosY = posY;
+                end
+            end
+            else begin
+                nextPosY = posY;
+            end
         end
-        else begin
-            nextPosY = posY;
-        end
-    end
-    else begin
-        nextPosY = posY;
-    end
+    endcase
 end
-
 
 endmodule
