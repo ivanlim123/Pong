f3933954ac50c94d9d5c345204775566df556b30
allen9613 <59288663+allen9613@users.noreply.github.com>
2019-12-30 22:26:21 +0800

    刪掉resetcollision ,v_cnt=480,

--- source/top.v
+++ source/top.v
@@ -39,7 +39,7 @@ inout PS2Clk;
  wire [9:0]posX2;
  wire [8:0]posY1;
  wire [8:0]posY2;
- wire valid;-
+ wire valid;
  wire [9:0]h_cnt;
  wire [9:0]v_cnt;
  wire [511:0] key_down;
@@ -119,7 +119,6 @@ onepulse o4(clk, de_enter, one_enter);
 /*wire R = BouncingObject | ball | (CounterX[3] ^ CounterY[3]);
 wire G = BouncingObject | ball;
 wire B = BouncingObject | ball;
-
 reg vga_R, vga_G, vga_B;
 always @(posedge clk)
 begin
@@ -137,23 +136,27 @@ wire paddle2 = ((CounterX>=posX2+8) && (CounterX<=posX2+18) &&(CounterY>=posY2+8
 wire BouncingObject = border | paddle1 | paddle2; // active if the border or paddle is redrawing itself
 */
 
-reg ResetCollision;
-always @(posedge clk) ResetCollision <= (v_cnt==500) & (h_cnt==0);  // active only once for every video frame
+//reg ResetCollision;
+//always @(posedge clk) ResetCollision <= (v_cnt==480) & (h_cnt==0);  // active only once for every video frame
 
 reg CollisionX1, CollisionX2, CollisionY1, CollisionY2;
-always @(posedge clk) if(ResetCollision) CollisionX1<=0; else if(BouncingObject & (h_cnt==ballX   ) & (v_cnt==ballY+ 4)) CollisionX1<=1;
+/*always @(posedge clk) if(ResetCollision) CollisionX1<=0; else if(BouncingObject & (h_cnt==ballX   ) & (v_cnt==ballY+ 4)) CollisionX1<=1;
 always @(posedge clk) if(ResetCollision) CollisionX2<=0; else if(BouncingObject & (h_cnt==ballX+8) & (v_cnt==ballY+ 4)) CollisionX2<=1;
 always @(posedge clk) if(ResetCollision) CollisionY1<=0; else if(BouncingObject & (h_cnt==ballX+ 4) & (v_cnt==ballY   )) CollisionY1<=1;
-always @(posedge clk) if(ResetCollision) CollisionY2<=0; else if(BouncingObject & (h_cnt==ballX+ 4) & (v_cnt==ballY+8)) CollisionY2<=1;
-Ball ball1(clk, rst,state,serve, 1'b0,1'b0,1'b0,1'b0, 1'b1, ballX, ballY,ballStatus);
+always @(posedge clk) if(ResetCollision) CollisionY2<=0; else if(BouncingObject & (h_cnt==ballX+ 4) & (v_cnt==ballY+8)) CollisionY2<=1;*/
+
+always @(*)if(BouncingObject & (h_cnt==ballX   ) & (v_cnt==ballY+ 4)) CollisionX1=1;else CollisionX1=0;
+always @(*)if(BouncingObject & (h_cnt==ballX+8) & (v_cnt==ballY+ 4)) CollisionX2=1;else CollisionX2=0;
+always @(*)if(BouncingObject & (h_cnt==ballX+ 4) & (v_cnt==ballY   )) CollisionY1=1;else CollisionY1=0;
+always @(*)if(BouncingObject & (h_cnt==ballX+ 4) & (v_cnt==ballY+8)) CollisionY2=1;else CollisionY2=0;
+
+Ball ball(clk, rst, state, serve, CollisionX1, CollisionX2, CollisionY1, CollisionY2, ballX, ballY, ballStatus);
 
 
 /*always @(posedge clk)
 if(ball_inX==0) ball_inX <= (h_cnt==ballX) & ball_inY; else ball_inX <= !(h_cnt==ballX+16);
-
 always @(posedge clk)
 if(ball_inY==0) ball_inY <= (v_cnt==ballY); else ball_inY <= !(v_cnt==ballY+16);
-
 wire ball = ball_inX & ball_inY;
   
   */
