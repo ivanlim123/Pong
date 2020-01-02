b6aef8473c46aeed911cf12cc86b0cac7605b48a
Ivan Lim <36753941+ivanlim123@users.noreply.github.com>
2020-01-02 18:34:41 +0800

    Add mode

--- source/Game.v
+++ source/Game.v
@@ -7,15 +7,21 @@
 `define PLAYER1WIN 2'b01
 `define PLAYER2WIN 2'b10
 
+`define PP 2'b00
+`define PA 2'b01
+`define AP 2'b10
+`define AA 2'b11
 
-module Game(clk, rst, ballStatus, enter, state, score1, score2, serve);
+module Game(clk, rst, ballStatus, change, enter, state, score1, score2, serve, mode);
 
 input clk, rst;
 input [1:0] ballStatus;
+input [3:0] change;
 input enter;
 output reg [1:0] state;
 output reg [1:0] score1, score2;
 output reg serve;
+output reg [1:0] mode;
 
 // score: 0-3
 
@@ -35,6 +41,8 @@ reg [1:0] nextState;
 reg [1:0] nextScore1, nextScore2;
 reg nextServe;
 
+reg [1:0] next_mode;
+
 // serve
 // 0: player1 serve
 // 1: player 2 serce
@@ -45,12 +53,14 @@ always @(posedge clk) begin
         score1 <= 2'd0;
         score2 <= 2'd0;
         serve <= 1'b0;
+        mode <= `PP;
     end
     else begin
         state <= nextState;
         score1 <= nextScore1;
         score2 <= nextScore2;
         serve <= nextServe;
+        mode <= next_mode;
     end
 end
 
@@ -61,11 +71,28 @@ always @(*) begin
             nextScore1 = 2'd0;
             nextScore2 = 2'd0;
             nextServe = 1'b0;
+            next_mode = mode;
         end
         `SERVE: begin
             nextScore1 = score1;
             nextScore2 = score2;
             nextServe = serve;
+            if(change==4'b1000) begin
+                next_mode = `PP;
+            end
+            else if(change==4'b0100) begin
+                next_mode = `PA;
+            end
+            else if(change==4'b0010) begin
+                next_mode = `AP;
+            end
+            else if(change==4'b0001) begin
+                next_mode = `AA;
+            end
+            else begin
+                next_mode = mode;
+            end
+
             if(enter==1'b1) begin
                 nextState = `PLAY;
             end
@@ -77,6 +104,7 @@ always @(*) begin
             nextScore1 = score1;
             nextScore2 = score2;
             nextServe = serve;
+            next_mode = mode;
             if(ballStatus==`PLAYING) begin
                 nextState = `PLAY;
             end
@@ -105,6 +133,7 @@ always @(*) begin
             nextScore1 = score1;
             nextScore2 = score2;
             nextServe = 1'b0;
+            next_mode = mode;
             if(enter==1'b1) begin
                 nextState = `START;
             end
