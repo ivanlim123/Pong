75c6b5d9fc45720cd37ad8b7314fb7d2f3f182af
Ivan Lim <36753941+ivanlim123@users.noreply.github.com>
2020-01-02 18:35:27 +0800

    Add mode AI and player

--- source/Player.v
+++ source/Player.v
@@ -4,13 +4,19 @@
 `define PLAY 2'b10
 `define DONE 2'b11
 
-module Player(clk, rst, state, keyboard, ballX, ballY, player, posX, posY);
+`define PP 2'b00
+`define PA 2'b01
+`define AP 2'b10
+`define AA 2'b11
+
+module Player(clk, rst, state, mode, keyboard, ballY, player, posX, posY);
 
 // declare input output
 input clk, rst, player;
 input [1:0] state;
+input [1:0] mode;
 input [1:0] keyboard;
-input [9:0] ballX;
+//input [9:0] ballX;
 input [9:0] ballY;
 output wire [9:0] posX;
 output reg [8:0] posY;
@@ -44,18 +50,24 @@ always @(*) begin
         end
         default: begin
             next_counter = counter + 1'b1;
-            if(counter==19'b111_1111_1111_1111_1111) begin
-                if(keyboard==2'b01) begin
-                    if(posY < 9'd424) begin
-                        nextPosY = posY + 1'b1;
+
+            if(mode==`PP) begin
+                if(counter==19'b111_1111_1111_1111_1111) begin
+                    if(keyboard==2'b01) begin
+                        if(posY < 9'd424) begin
+                            nextPosY = posY + 1'b1;
+                        end
+                        else begin
+                            nextPosY = posY;
+                        end
                     end
-                    else begin
-                        nextPosY = posY;
-                    end
-                end
-                else if(keyboard==2'b10) begin
-                    if(posY > 9'd0) begin
-                        nextPosY = posY - 1'b1;
+                    else if(keyboard==2'b10) begin
+                        if(posY > 9'd0) begin
+                            nextPosY = posY - 1'b1;
+                        end
+                        else begin
+                            nextPosY = posY;
+                        end
                     end
                     else begin
                         nextPosY = posY;
@@ -65,8 +77,94 @@ always @(*) begin
                     nextPosY = posY;
                 end
             end
+            else if(mode==`PA) begin
+                if(player==1'b0) begin
+                    if(counter==19'b111_1111_1111_1111_1111) begin
+                        if(keyboard==2'b01) begin
+                            if(posY < 9'd424) begin
+                                nextPosY = posY + 1'b1;
+                            end
+                            else begin
+                                nextPosY = posY;
+                            end
+                        end
+                        else if(keyboard==2'b10) begin
+                            if(posY > 9'd0) begin
+                                nextPosY = posY - 1'b1;
+                            end
+                            else begin
+                                nextPosY = posY;
+                            end
+                        end
+                        else begin
+                            nextPosY = posY;
+                        end
+                    end
+                    else begin
+                        nextPosY = posY;
+                    end
+                end
+                else begin
+                    if(ballY+4>posY+20 && posY < 9'd424) begin
+                        nextPosY = posY + 1;
+                    end
+                    else if(ballY+4<posY+20 && posY > 9'd0) begin
+                        nextPosY = posY - 1;
+                    end
+                    else begin
+                        nextPosY = posY;
+                    end
+                end
+            end
+            else if(mode==`AP) begin
+                if(player==1'b1) begin
+                    if(counter==19'b111_1111_1111_1111_1111) begin
+                        if(keyboard==2'b01) begin
+                            if(posY < 9'd424) begin
+                                nextPosY = posY + 1'b1;
+                            end
+                            else begin
+                                nextPosY = posY;
+                            end
+                        end
+                        else if(keyboard==2'b10) begin
+                            if(posY > 9'd0) begin
+                                nextPosY = posY - 1'b1;
+                            end
+                            else begin
+                                nextPosY = posY;
+                            end
+                        end
+                        else begin
+                            nextPosY = posY;
+                        end
+                    end
+                    else begin
+                        nextPosY = posY;
+                    end
+                end
+                else begin
+                    if(ballY+4>posY+20 && posY < 9'd424) begin
+                        nextPosY = posY + 1;
+                    end
+                    else if(ballY+4<posY+20 && posY > 9'd0) begin
+                        nextPosY = posY - 1;
+                    end
+                    else begin
+                        nextPosY = posY;
+                    end
+                end
+            end
             else begin
-                nextPosY = posY;
+                if(ballY+4>posY+20 && posY < 9'd424) begin
+                    nextPosY = posY + 1;
+                end
+                else if(ballY+4<posY+20 && posY > 9'd0) begin
+                    nextPosY = posY - 1;
+                end
+                else begin
+                    nextPosY = posY;
+                end
             end
         end
     endcase
