897305acc37c4b5d00c728917c2a64725dbdb6ca
Ivan Lim <36753941+ivanlim123@users.noreply.github.com>
2020-01-09 04:55:11 +0800

    ibeat count

--- source/PlayerCtrl.v
+++ source/PlayerCtrl.v
@@ -1,74 +1,43 @@
-`define STOP 2'b00
-`define WIN 2'b01
-`define COLLIDE 2'b10
-
-`define PLAYING 2'b00
-`define PLAYER1WIN 2'b01
-`define PLAYER2WIN 2'b10
-
-
-module PlayerCtrl (
+module PlayerCtrl1 (
 	input clk,
 	input reset,
-	input [1:0] ballStatus,
-	input CollisionX1, 
-	input CollisionX2, 
-	input CollisionY1, 
-	input CollisionY2,
+	input en,
 	output reg [7:0] ibeat
 );
-
-reg [1:0] state, next_state;
-reg [7:0] next_ibeat;
+parameter BEATLEAGTH = 6;
 
 always @(posedge clk, posedge reset) begin
 	if (reset) begin
-		state <= `STOP;
-		ibeat <= 8'd0;
+		ibeat <= 0;
+    end
+	else if ((ibeat < BEATLEAGTH) && en) begin 
+		ibeat <= ibeat + 1;
 	end
-	else begin
-		state <= next_state;
-		ibeat <= next_ibeat;
+	else begin 
+		ibeat <= 0;
 	end
 end
 
-always @(*) begin
-    if((ballStatus == `PLAYER1WIN || ballStatus == `PLAYER2WIN) && ibeat == 8'd0) begin
-        next_state = `WIN;
-        next_ibeat = 8'd8;
+endmodule
+
+module PlayerCtrl2 (
+	input clk,
+	input reset,
+	input [2:0] en,
+	output reg [7:0] ibeat
+);
+parameter BEATLEAGTH = 7;
+
+always @(posedge clk, posedge reset) begin
+	if (reset) begin
+		ibeat <= 0;
     end
-    else begin
-        if((CollisionX1 == 1'b1 || CollisionX2 == 1'b1 || CollisionY1 == 1'b1 || CollisionY2 == 1'b1) && ibeat == 8'd0) begin
-            next_state = `COLLIDE;
-            next_ibeat = 8'd6;
-        end
-        else begin
-            if(state == `WIN) begin
-                if(ibeat == 8'd13) begin
-                    next_state = `STOP;
-                    next_ibeat = 8'd0;
-                end
-                else begin
-                    next_state = `WIN;
-                    next_ibeat = ibeat + 1'b1;
-                end
-            end
-            else if(state == `COLLIDE) begin
-                if(ibeat == 8'd7) begin
-                    next_state = `STOP;
-                    next_ibeat = 8'd0;
-                end
-                else begin
-                    next_state = `COLLIDE;
-                    next_ibeat = ibeat + 1'b1;
-                end
-            end
-            else begin
-                next_state = `STOP;
-                next_ibeat = 8'd0;
-            end
-        end
-    end
+	else if ((ibeat < BEATLEAGTH) && en) begin 
+		ibeat <= ibeat + 1;
+	end
+	else begin 
+		ibeat <= 0;
+	end
 end
 
 endmodule
